6f5221c08a594b63f26900f6ff998c9e15db44db
mmirzay <mirzay.mohsen@gmail.com>
2022-01-17 11:57:39 +0330

    revised bank repository.

--- src/com/project/my/homeworks/hw6/q4/bakend/repositories/BankRepository.java
+++ src/com/project/my/homeworks/hw6/q4/bakend/repositories/BankRepository.java
@@ -4,13 +4,15 @@ import com.project.my.homeworks.hw6.q4.bakend.entities.banks.Bank;
 import com.project.my.homeworks.hw6.q4.bakend.exceptions.BankException;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BankRepository {
-    private List<Bank> banks;
+    private Set<Bank> banks;
 
     public BankRepository() {
-        banks = new ArrayList<>();
+        banks = new HashSet<>();
         initBanksList();
     }
 
@@ -21,13 +23,17 @@ public class BankRepository {
         banks.add(new Bank("Saderat", 19));
     }
 
-    public Bank getBankByCode(int code) throws BankException {
+    public List<Bank> fetchAllBanks() {
+        return new ArrayList<>(banks);
+    }
+
+    public Bank fetchBankByCode(int code) throws BankException {
         return banks.stream()
                 .filter(b -> b.getCode() == code)
                 .findFirst().orElseThrow(() -> new BankException("bank code is not exist."));
     }
 
-    public Bank getBankCodeByName(String name) throws BankException {
+    public Bank fetchBankByName(String name) throws BankException {
         return banks.stream()
                 .filter(b -> b.getName().equals(name))
                 .findFirst().orElseThrow(() -> new BankException("bank name is not exist."));
