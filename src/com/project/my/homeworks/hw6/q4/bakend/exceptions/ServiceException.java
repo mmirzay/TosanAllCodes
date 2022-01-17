84f319e0e48a25b51528d939df8e3571ef3417c4
mmirzay <mirzay.mohsen@gmail.com>
2022-01-17 17:25:47 +0330

    added service exception class.

---
+++ src/com/project/my/homeworks/hw6/q4/bakend/exceptions/ServiceException.java
@@ -0,0 +1,7 @@
+package com.project.my.homeworks.hw6.q4.bakend.exceptions;
+
+public class ServiceException extends Exception {
+    public ServiceException(Throwable e) {
+        super(e);
+    }
+}
