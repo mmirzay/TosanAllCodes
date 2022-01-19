a07f1e9b572b1f899c64f757e74b1bc4d873d63f
mmirzay <mirzay.mohsen@gmail.com>
2022-01-19 23:59:11 +0330

    revised main class to run the program.

--- src/com/project/my/homeworks/hw6/q4/Main.java
+++ src/com/project/my/homeworks/hw6/q4/Main.java
@@ -1,7 +1,11 @@
 package com.project.my.homeworks.hw6.q4;
 
+import com.project.my.homeworks.hw6.q4.frontend.UiController;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Simple Bank System");
+        UiController uiController = new UiController();
+        uiController.run();
     }
 }
