dbae60b7a3190d5a1cfd84a4b347f2e5d4b1a7ed
mmirzay <mirzay.mohsen@gmail.com>
2022-01-19 23:51:35 +0330

    added  Account services.

--- src/com/project/my/homeworks/hw6/q4/bakend/api/BankManagerInterface.java
+++ src/com/project/my/homeworks/hw6/q4/bakend/api/BankManagerInterface.java
@@ -1,5 +1,6 @@
 package com.project.my.homeworks.hw6.q4.bakend.api;
 
+import com.project.my.homeworks.hw6.q4.bakend.entities.accounts.Account;
 import com.project.my.homeworks.hw6.q4.bakend.entities.banks.Bank;
 import com.project.my.homeworks.hw6.q4.bakend.entities.users.Customer;
 import com.project.my.homeworks.hw6.q4.bakend.exceptions.ServiceException;
@@ -17,4 +18,20 @@ public interface BankManagerInterface {
     List<Customer> getAllCustomers();
 
     Customer getCustomerById(long nationalId) throws ServiceException;
+
+    void addAccount(Account account);
+
+    List<Account> getAllAccounts();
+
+    List<Account> getAllAccountsOfCustomer(long nationalId);
+
+    List<Account> getAllAccountsOfBank(int bankCode);
+
+    List<Account> getAllAccountsOfCustomerInBank(long nationalId, int bankCode);
+
+    Account getAccountByNumber(long accountNumber) throws ServiceException;
+
+    boolean closeAccount(long accountNumber);
+
+    boolean reopenAccount(long accountNumber);
 }
--- src/com/project/my/homeworks/hw6/q4/bakend/services/BankManagerService.java
+++ src/com/project/my/homeworks/hw6/q4/bakend/services/BankManagerService.java
@@ -1,8 +1,10 @@
 package com.project.my.homeworks.hw6.q4.bakend.services;
 
 import com.project.my.homeworks.hw6.q4.bakend.api.BankManagerInterface;
+import com.project.my.homeworks.hw6.q4.bakend.entities.accounts.Account;
 import com.project.my.homeworks.hw6.q4.bakend.entities.banks.Bank;
 import com.project.my.homeworks.hw6.q4.bakend.entities.users.Customer;
+import com.project.my.homeworks.hw6.q4.bakend.exceptions.AccountException;
 import com.project.my.homeworks.hw6.q4.bakend.exceptions.BankException;
 import com.project.my.homeworks.hw6.q4.bakend.exceptions.CustomerException;
 import com.project.my.homeworks.hw6.q4.bakend.exceptions.ServiceException;
@@ -13,6 +15,7 @@ import java.util.List;
 public class BankManagerService implements BankManagerInterface {
     private final BankRepository bankRepository = new BankRepository();
     private final CustomerRepository customerRepository = new CustomerRepository();
+    private final AccountRepository accountRepository = new AccountRepository();
 
     @Override
     public List<Bank> getAllBanksList() {
@@ -44,6 +47,72 @@ public class BankManagerService implements BankManagerInterface {
             return customerRepository.fetchCustomerById(nationalId);
         } catch (CustomerException e) {
             throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public void addAccount(Account account) {
+        accountRepository.insertAccount(account);
+    }
+
+    @Override
+    public List<Account> getAllAccounts() {
+        return accountRepository.fetchAllAccounts();
+    }
+
+    @Override
+    public List<Account> getAllAccountsOfCustomer(long nationalId) {
+        return accountRepository.fetchAllAccountsOfCustomer(nationalId);
+    }
+
+    @Override
+    public List<Account> getAllAccountsOfBank(int bankCode) {
+        return accountRepository.fetchAllAccountsOfBank(bankCode);
+    }
+
+    @Override
+    public List<Account> getAllAccountsOfCustomerInBank(long nationalId, int bankCode) {
+        return accountRepository.fetchAccountOfCustomerInBank(nationalId, bankCode);
+    }
+
+    @Override
+    public Account getAccountByNumber(long accountNumber) throws ServiceException {
+        try {
+            return accountRepository.fetchAccountByNumber(accountNumber);
+        } catch (AccountException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    private Account getAccountOfCustomerInBankByAccountNumber(long nationalId, int bankCode, long accountNumber) {
+        return getAllAccountsOfCustomerInBank(nationalId, bankCode).stream().filter(a -> a.getNumber() == accountNumber).findFirst().orElse(null);
+    }
+
+    private Account getAccountInBankByAccountNumber(int bankCode, long accountNumber) {
+        return getAllAccountsOfBank(bankCode).stream().filter(a -> a.getNumber() == accountNumber).findFirst().orElse(null);
+    }
+
+    @Override
+    public boolean closeAccount(long accountNumber) {
+        try {
+            Account account = accountRepository.fetchAccountByNumber(accountNumber);
+            account.setClosed();
+            accountRepository.updateAccount(account);
+            return true;
+        } catch (AccountException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean reopenAccount(long accountNumber) {
+        try {
+            Account account = accountRepository.fetchAccountByNumber(accountNumber);
+            account.setOpened();
+            accountRepository.updateAccount(account);
+            return true;
+        } catch (AccountException e) {
+            return false;
         }
     }
 }
