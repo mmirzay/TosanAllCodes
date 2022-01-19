4a601c59eba636cf08032691a125d8fe0cdc377c
mmirzay <mirzay.mohsen@gmail.com>
2022-01-19 23:53:31 +0330

    added credit card services.

--- src/com/project/my/homeworks/hw6/q4/bakend/api/BankManagerInterface.java
+++ src/com/project/my/homeworks/hw6/q4/bakend/api/BankManagerInterface.java
@@ -1,13 +1,16 @@
 package com.project.my.homeworks.hw6.q4.bakend.api;
 
 import com.project.my.homeworks.hw6.q4.bakend.entities.accounts.Account;
+import com.project.my.homeworks.hw6.q4.bakend.entities.accounts.CreditCard;
 import com.project.my.homeworks.hw6.q4.bakend.entities.banks.Bank;
+import com.project.my.homeworks.hw6.q4.bakend.entities.transactions.*;
 import com.project.my.homeworks.hw6.q4.bakend.entities.users.Customer;
 import com.project.my.homeworks.hw6.q4.bakend.exceptions.ServiceException;
 
 import java.util.List;
 
 public interface BankManagerInterface {
+
 
     List<Bank> getAllBanksList();
 
@@ -34,4 +37,23 @@ public interface BankManagerInterface {
     boolean closeAccount(long accountNumber);
 
     boolean reopenAccount(long accountNumber);
+
+    void addCreditCard(CreditCard creditCard);
+
+    List<CreditCard> getAllCreditCards();
+
+    List<CreditCard> getAllCreditCardsOfCustomer(long nationalId);
+
+    List<CreditCard> getAllCreditCardsOfBank(int bankCode);
+
+    List<CreditCard> getAllCreditCardsOfCustomerInBank(long nationalId, int bankCode);
+
+    CreditCard getCreditCardByAccountNumber(long accountNumber) throws ServiceException;
+
+    CreditCard getCreditCardByCardNumber(String cardNumber) throws ServiceException;
+
+    CreditCard getCustomerCreditCardByCardNumber(long nationalId, String sourceCardNumber) throws ServiceException;
+
+    boolean reactivateCreditCard(String cardNumber);
+
 }
--- src/com/project/my/homeworks/hw6/q4/bakend/services/BankManagerService.java
+++ src/com/project/my/homeworks/hw6/q4/bakend/services/BankManagerService.java
@@ -2,20 +2,20 @@ package com.project.my.homeworks.hw6.q4.bakend.services;
 
 import com.project.my.homeworks.hw6.q4.bakend.api.BankManagerInterface;
 import com.project.my.homeworks.hw6.q4.bakend.entities.accounts.Account;
+import com.project.my.homeworks.hw6.q4.bakend.entities.accounts.CreditCard;
 import com.project.my.homeworks.hw6.q4.bakend.entities.banks.Bank;
 import com.project.my.homeworks.hw6.q4.bakend.entities.users.Customer;
-import com.project.my.homeworks.hw6.q4.bakend.exceptions.AccountException;
-import com.project.my.homeworks.hw6.q4.bakend.exceptions.BankException;
-import com.project.my.homeworks.hw6.q4.bakend.exceptions.CustomerException;
-import com.project.my.homeworks.hw6.q4.bakend.exceptions.ServiceException;
+import com.project.my.homeworks.hw6.q4.bakend.exceptions.*;
 import com.project.my.homeworks.hw6.q4.bakend.repositories.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BankManagerService implements BankManagerInterface {
     private final BankRepository bankRepository = new BankRepository();
     private final CustomerRepository customerRepository = new CustomerRepository();
     private final AccountRepository accountRepository = new AccountRepository();
+    private final CreditCardRepository creditCardRepository = new CreditCardRepository();
 
     @Override
     public List<Bank> getAllBanksList() {
@@ -112,6 +112,96 @@ public class BankManagerService implements BankManagerInterface {
             accountRepository.updateAccount(account);
             return true;
         } catch (AccountException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public void addCreditCard(CreditCard creditCard) {
+        creditCardRepository.insertCreditCard(creditCard);
+    }
+
+    @Override
+    public List<CreditCard> getAllCreditCards() {
+        return creditCardRepository.fetchAllCreditCards();
+    }
+
+    @Override
+    public List<CreditCard> getAllCreditCardsOfCustomer(long nationalId) {
+        List<CreditCard> result = new ArrayList<>();
+        for (Account account : accountRepository.fetchAllAccountsOfCustomer(nationalId)) {
+            try {
+                result.add(creditCardRepository.fetchCreditCardOfAccountNumber(account.getNumber()));
+            } catch (CreditCardException e) {
+                continue;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public List<CreditCard> getAllCreditCardsOfBank(int bankCode) {
+        List<CreditCard> result = new ArrayList<>();
+        for (Account account : accountRepository.fetchAllAccountsOfBank(bankCode))
+            try {
+                result.add(creditCardRepository.fetchCreditCardOfAccountNumber(account.getNumber()));
+            } catch (CreditCardException e) {
+                continue;
+            }
+
+        return result;
+    }
+
+    @Override
+    public List<CreditCard> getAllCreditCardsOfCustomerInBank(long nationalId, int bankCode) {
+        List<CreditCard> result = new ArrayList<>();
+        for (Account account : accountRepository.fetchAccountOfCustomerInBank(nationalId, bankCode))
+            try {
+                result.add(creditCardRepository.fetchCreditCardOfAccountNumber(account.getNumber()));
+            } catch (CreditCardException e) {
+                continue;
+            }
+
+        return result;
+    }
+
+    @Override
+    public CreditCard getCreditCardByAccountNumber(long accountNumber) throws ServiceException {
+        try {
+            return creditCardRepository.fetchCreditCardOfAccountNumber(accountNumber);
+        } catch (CreditCardException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public CreditCard getCreditCardByCardNumber(String cardNumber) throws ServiceException {
+        try {
+            return creditCardRepository.fetchCreditCardByNumber(cardNumber);
+        } catch (CreditCardException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public CreditCard getCustomerCreditCardByCardNumber(long nationalId, String cardNumber) throws ServiceException {
+        try {
+            return getAllCreditCardsOfCustomer(nationalId).stream()
+                    .filter(c -> c.getNumber().equals(cardNumber)).findFirst()
+                    .orElseThrow(() -> new CreditCardException("credit card number is not exist"));
+        } catch (CreditCardException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public boolean reactivateCreditCard(String cardNumber) {
+        try {
+            CreditCard creditCard = creditCardRepository.fetchCreditCardByNumber(cardNumber);
+            creditCard.setActive();
+            creditCardRepository.updateCreditCard(creditCard);
+            return true;
+        } catch (CreditCardException e) {
             return false;
         }
     }
