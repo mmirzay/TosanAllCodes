9a08479d8732519ac32140af894ee257c012ad8b
mmirzay <mirzay.mohsen@gmail.com>
2022-01-19 23:48:57 +0330

    added customer services.

--- src/com/project/my/homeworks/hw6/q4/bakend/api/BankManagerInterface.java
+++ src/com/project/my/homeworks/hw6/q4/bakend/api/BankManagerInterface.java
@@ -1,12 +1,20 @@
 package com.project.my.homeworks.hw6.q4.bakend.api;
 
 import com.project.my.homeworks.hw6.q4.bakend.entities.banks.Bank;
+import com.project.my.homeworks.hw6.q4.bakend.entities.users.Customer;
 import com.project.my.homeworks.hw6.q4.bakend.exceptions.ServiceException;
 
 import java.util.List;
 
 public interface BankManagerInterface {
+
     List<Bank> getAllBanksList();
 
     Bank getBankByCode(int bankCode) throws ServiceException;
+
+    void addCustomer(Customer customer);
+
+    List<Customer> getAllCustomers();
+
+    Customer getCustomerById(long nationalId) throws ServiceException;
 }
--- src/com/project/my/homeworks/hw6/q4/bakend/services/BankManagerService.java
+++ src/com/project/my/homeworks/hw6/q4/bakend/services/BankManagerService.java
@@ -2,7 +2,9 @@ package com.project.my.homeworks.hw6.q4.bakend.services;
 
 import com.project.my.homeworks.hw6.q4.bakend.api.BankManagerInterface;
 import com.project.my.homeworks.hw6.q4.bakend.entities.banks.Bank;
+import com.project.my.homeworks.hw6.q4.bakend.entities.users.Customer;
 import com.project.my.homeworks.hw6.q4.bakend.exceptions.BankException;
+import com.project.my.homeworks.hw6.q4.bakend.exceptions.CustomerException;
 import com.project.my.homeworks.hw6.q4.bakend.exceptions.ServiceException;
 import com.project.my.homeworks.hw6.q4.bakend.repositories.*;
 
@@ -10,6 +12,7 @@ import java.util.List;
 
 public class BankManagerService implements BankManagerInterface {
     private final BankRepository bankRepository = new BankRepository();
+    private final CustomerRepository customerRepository = new CustomerRepository();
 
     @Override
     public List<Bank> getAllBanksList() {
@@ -21,6 +24,25 @@ public class BankManagerService implements BankManagerInterface {
         try {
             return bankRepository.fetchBankByCode(bankCode);
         } catch (BankException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public void addCustomer(Customer customer) {
+        customerRepository.insertCustomer(customer);
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return customerRepository.fetchAllCustomers();
+    }
+
+    @Override
+    public Customer getCustomerById(long nationalId) throws ServiceException {
+        try {
+            return customerRepository.fetchCustomerById(nationalId);
+        } catch (CustomerException e) {
             throw new ServiceException(e);
         }
     }
