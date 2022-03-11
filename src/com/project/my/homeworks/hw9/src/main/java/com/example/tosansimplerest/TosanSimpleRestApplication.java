e2d89839795817218f0d5035908023207225cbc7
mmirzay <mirzay.mohsen@gmail.com>
2022-03-11 14:55:57 +0330

    added new project for hw 9 as a simple rest banking app.

---
+++ src/com/project/my/homeworks/hw9/src/main/java/com/example/tosansimplerest/TosanSimpleRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.tosansimplerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TosanSimpleRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TosanSimpleRestApplication.class, args);
+    }
+
+}
---
+++ src/com/project/my/homeworks/hw9/src/test/java/com/example/tosansimplerest/TosanSimpleRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tosansimplerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TosanSimpleRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
