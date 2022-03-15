625516ad5789d135855aded46528ad2d69fdde19
mmirzay <mirzay.mohsen@gmail.com>
2022-03-15 04:18:48 +0330

    revised deposit entity.

--- src/com/project/my/homeworks/hw9/src/main/java/com/example/tosansimplerest/backend/entities/Deposit.java
+++ src/com/project/my/homeworks/hw9/src/main/java/com/example/tosansimplerest/backend/entities/Deposit.java
@@ -1,14 +1,39 @@
 package com.example.tosansimplerest.backend.entities;
 
-import lombok.AllArgsConstructor;
+import com.example.tosansimplerest.backend.utilities.DepositNumberGenerator;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
+@Entity
 public class Deposit {
-    private String number;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private long number;
     private Status status;
     private double balance;
+    @ManyToOne(optional = false)
+    Customer owner;
+
+    private Deposit(long number, Status status, double balance, Customer owner) {
+        this.number = number;
+        this.status = status;
+        this.balance = balance;
+        this.owner = owner;
+    }
+
+    public static Deposit of(Customer owner) {
+        return of(owner, 0);
+    }
+
+    public static Deposit of(Customer owner, double balance) {
+        long number = DepositNumberGenerator.getRandomAccountNumber();
+        Status status = Status.OPENED;
+        return new Deposit(number, status, balance, owner);
+    }
 }
